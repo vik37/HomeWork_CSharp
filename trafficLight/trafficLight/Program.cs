a46f591715efc72850c3587ccc9ae5329a90e445
vik37 <vik.zafirovski@gmail.com>
2020-03-05 18:26:02 +0100

    add_homework01

---
+++ trafficLight/trafficLight/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace trafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Stop");
+            
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ready");
+            
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Go");
+            Console.ReadLine();
+        }
+    }
+}

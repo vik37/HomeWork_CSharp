849303bd3cf7bf5765b0c33738c1bf0353b995be
vik37 <vik.zafirovski@gmail.com>
2020-03-13 04:44:03 +0100

    Homework_03 if_else

---
+++ homework_03/homework_03/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace homework_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two variables and initialize them with a keyboard input
+            //Console.WriteLine("Input number. I don't care what number. Please input some number ok? ");
+            //string firstNum = Console.ReadLine();
+            //int parsNumFirst = int.Parse(firstNum);
+
+            //string secondNum = Console.ReadLine();
+            //int parsNumSecond = int.Parse(secondNum);
+            // Write code that can test which number is larger
+            //if (parsNumFirst > parsNumSecond)
+            //{
+            //    Console.WriteLine(parsNumFirst + " is larger then " + parsNumSecond);
+            //}
+
+            //else if (parsNumFirst < parsNumSecond)
+            //{
+            //    Console.WriteLine(parsNumSecond + " is larger then " + parsNumFirst);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(parsNumFirst + " is equal to " + parsNumSecond);
+            //}
+            //Write code that can test the numbers whether they are even or odd
+
+            //if (parsNumFirst % 2 == 0)
+            //{
+            //    Console.WriteLine(parsNumFirst + "  is EVEN");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(parsNumFirst + "  is ODD");
+            //}
+            //if (parsNumSecond % 2 == 0)
+            //{
+            //    Console.WriteLine(parsNumSecond + "  is EVEN");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(parsNumSecond + "  is ODD");
+            //}
+            // Console.ReadLine();
+
+            //Write the larger number from the two in the console
+
+            int num = 3;
+
+            if (num > 2)
+            {
+                Console.WriteLine(num);
+                // After that write if the number is even or odd
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("EVEN");
+                }
+                else
+                {
+                    Console.WriteLine("ODD");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR!!!");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework_03/hwork0302/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace hwork0302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solve the following problem. On one tree there are 12 branches with n apples on them. 
+            //One basket can hold m apples. If a user enters number of trees find out how many baskets 
+            //    does it take to collect all the apples from the trees?
+            //n = 8
+            //m = 5
+            //Result = ?
+
+
+
+            int n = 8;
+            double m = 5;
+            string treeInput = Console.ReadLine();
+            double tree = double.Parse(treeInput);
+
+            double result = n * tree / m;
+
+            Int32 resultTwo = Convert.ToInt32(result);
+
+
+                Console.WriteLine(result);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}

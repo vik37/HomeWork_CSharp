956c86863b70a7960cd2c23fdfe33c7f6ae622b1
vik37 <59177404+vik37@users.noreply.github.com>
2020-03-14 01:48:00 +0100

    homework_04_loops

---
+++ homework_04/homwork0402/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace homwork0402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get an input from the console
+            string strNumOne = Console.ReadLine();
+            int numOne = int.Parse(strNumOne);
+            int count = 2;
+
+            // Print all even numbers from the console
+            while (count < numOne)  
+            {
+                if (count % 2 == 0)
+                {
+                    Console.WriteLine(count);
+                }
+
+                count++;
+            }
+            Console.ReadLine();
+
+            //Get another number from the console
+
+            //string strNumTwo = Console.ReadLine();
+            //int numTwo = int.Parse(strNumTwo);
+
+            //Print all odd numbers starting from 1
+
+            //int count = 0;
+            //while(count < numTwo)
+            //{
+            //    if(count % 2 != 0)
+            //    {
+            //        Console.WriteLine(count);
+
+            //    }
+            //    count++;
+            //}
+            //Console.ReadLine();
+        }
+    }
+ }
---
+++ homework_04/prpject_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace prpject_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string strNum = Console.ReadLine();
+            //int num = int.Parse(strNum);
+            //int count = 1;
+
+            //while(count <= num)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //Console.ReadLine();
+
+            int m = 0;
+            string numAa = Console.ReadLine();
+            int a = int.Parse(numAa);
+            while (a > m)
+            {
+                Console.WriteLine(a);
+                a--;
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}

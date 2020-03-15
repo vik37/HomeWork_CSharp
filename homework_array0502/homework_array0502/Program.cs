c17d32c4f8a40a7854983b3d08a20f45abacce00
vik37 <59177404+vik37@users.noreply.github.com>
2020-03-15 01:43:51 +0100

    HomeWork_Array0502

---
+++ homework_array0502/homework_array0502/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace homework_array0502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { Console.ReadLine() }; 
+
+     
+            string[] names;
+
+
+            //do
+            //{
+            //    Console.WriteLine("Enter the name");
+            //    names = new string[] { Console.ReadLine() };
+
+
+            //    Console.WriteLine("Do you want to enter another name? (Y / N)");
+            //    string yesNo = Console.ReadLine();
+            //    Char parsYesno = Char.Parse(yesNo);
+            //    //if (parsYesno == 'N')
+            //    //{
+            //    //    break;
+            //    //}
+
+
+            //}
+            //while (parsYesno == 'N');
+
+            //for(int i = 0; i<names.Length;i++)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.ReadLine();
+            //}
+
+
+            
+            while (true) 
+            {
+                Console.WriteLine("Enter the name");
+                names = new string[] { Console.ReadLine() };
+
+
+                Console.WriteLine("Do you want to enter another name? (Y / N)");
+                string yesNo = Console.ReadLine();
+                Char parsYesno = Char.Parse(yesNo);
+                if (parsYesno == 'N')
+                {
+                    break;
+                }
+
+
+            }
+
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}

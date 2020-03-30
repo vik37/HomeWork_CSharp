0abcfc4691a528a44dc6bd3920400876169de084
vik37 <59177404+vik37@users.noreply.github.com>
2020-03-30 18:44:43 +0200

    homework_string_data

---
+++ Homework_strings_date_ObjectClass/HomeWork_Date/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HomeWork_Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 5
+            //*Display the Day properties (year, month, day, hour, minute, second) from the today's date. *Expected Output : *year = 2020
+            //*month = 3
+            //*day = 26
+            //*hour = 23
+            //*minute = 12
+            //*second = 22 
+
+            // var today = DateTime.Now;
+
+            //int year = today.Year;
+            //int month = today.Month;
+            //int day = today.Day;
+            //int hour = today.Hour;
+            //int minute = today.Minute;
+            //int second = today.Second;
+
+            //string s = " *year = {0}, \n *month = {1}, \n *day = {2}, \n *hour = {3}, \n *minute = {4}, \n *second = {5}";
+            //Console.WriteLine(s,year,month,day,hour,minute,second);
+            //Console.ReadLine();
+
+
+            //  Task 6
+            // * Find the leap years between 2008 and 2020. * Expected output: *2008 is a leap year. * 2012 is a leap year. * 2016 is a leap year. * 2020 is a leap year.
+
+            //* Hint - make some researches of DateTime methods, you will find interesting ones that might help you;)
+
+            // PRV NACIN 
+            var today = DateTime.Now;
+            int year = today.Year;
+            int yearTwoThousand = 2008;
+            int four = 4;
+            
+            for(int i = yearTwoThousand; i <= year; i++)
+            {
+                Console.WriteLine(i + " is a leap year");
+                i = i + four -1;
+            }
+            Console.ReadLine();
+
+            // VTOR NACIN
+            for (int y = 2000; y <= 2010; y++)
+            {
+                if (DateTime.IsLeapYear(y))
+                {
+                    Console.WriteLine("{0} is a Leap Year.", y);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_strings_date_ObjectClass/Homework_strings_date_ObjectClass/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Homework_strings_date_ObjectClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // HomeWork - Strings
+            /* Task 1
+             *Print individual characters of the string in reverse order. *Example: we love c# 
+             * *Expected Output : #c evol ew */
+
+            //string cSharp = "we love c#";
+            //char[] charsSharp = cSharp.ToCharArray();
+            //Array.Reverse(charsSharp);
+
+            //Console.WriteLine(charsSharp);
+            //Console.ReadLine();
+
+
+            // Task 2
+            /* *Count the total number of words in a string entered by the user. *Example: user enters 
+            "I love C#" *Expected output: "I love c#" contains 3 words. */
+
+            //string userWords = Console.ReadLine();
+            //string[] splitWord = userWords.Split(" ");
+            //int lengthOfWords = splitWord.Length;
+
+            //Console.WriteLine(lengthOfWords);
+            //Console.ReadLine();
+
+
+            // Task 3
+            /* *Find maximum occurring character in a string *Example:
+             * "We want this situation with covid-19 to ends!" *Expected Output : 
+             * The highest frequency of character 't' appears number of times : 6 */
+
+            string covidNintheen = "We want this situation with covid-19 to ends!";
+            char[] covidChar = covidNintheen.ToCharArray();
+            
+            int[] count = new int[covidChar.Length];
+            int i, j; 
+
+            for (i = 0; i < covidChar.Length; i++)
+            {
+                int c = 0;
+                for (j = 0; j < covidChar.Length; j++)
+                {
+                    if (covidChar[i] == covidChar[j])
+                    {
+                        
+                        c++;                        
+                    }
+                }
+                count[i] = c;
+            }
+            
+            int max = 0;
+            int maxTimes = 0;
+            for(i = 0;i < count.Length; i++)
+            {
+                if(count[i] > max)
+                {
+                    max = Array.IndexOf(count, count[i]);
+                    maxTimes = count[i];
+                    
+                }
+            }
+          
+           
+            Console.WriteLine("Number of same letter is {0}", maxTimes);
+            
+            Console.ReadLine();
+
+
+            /*  Task 4
+             "The whole group of G1 loves C#, we are sure that with their deducation and passion they will be successfull programers soon!"
+
+            * Print the whole text after "," in the console. *Bonus - Try to do that without counting the characters till "," by yourself :) */
+
+            //string groupG1 = "The whole group of G1 loves C#, we are sure that with their deducation and passion they will be successfull programers soon!";
+            //string[] splitGone = groupG1.Split(", ");
+
+
+            //Console.WriteLine(@"""No bonus"" - {0} ", splitGone[1]);
+            //Console.WriteLine("----------------");
+            //int indexOfGroupOne = groupG1.IndexOf("we are sure that with their deducation and passion they will be successfull programers soon!");
+            //string newString = groupG1.Substring(indexOfGroupOne);
+
+            //Console.WriteLine(@"""Bonus"" - {0} ", newString);
+            //Console.ReadLine();
+        }
+    }
+}

1c6c5fe07d044aac8cdc87afaa49f0adce90802c
vik37 <59177404+vik37@users.noreply.github.com>
2020-05-30 23:21:03 +0200

    My Fist WinForm

---
+++ First_Win/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ClickMe = new System.Windows.Forms.Button();
+            this.Hello_textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClickMe
+            // 
+            this.ClickMe.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClickMe.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ClickMe.BackgroundImage")));
+            this.ClickMe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClickMe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ClickMe.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClickMe.FlatAppearance.BorderSize = 15;
+            this.ClickMe.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClickMe.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.ClickMe.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ClickMe.ForeColor = System.Drawing.Color.DimGray;
+            this.ClickMe.Location = new System.Drawing.Point(340, 86);
+            this.ClickMe.Name = "ClickMe";
+            this.ClickMe.Size = new System.Drawing.Size(256, 75);
+            this.ClickMe.TabIndex = 0;
+            this.ClickMe.Text = "Click Me";
+            this.ClickMe.UseVisualStyleBackColor = false;
+            this.ClickMe.Click += new System.EventHandler(this.ClickMe_Click);
+            // 
+            // Hello_textBox1
+            // 
+            this.Hello_textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Hello_textBox1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Hello_textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.Hello_textBox1.Location = new System.Drawing.Point(157, 28);
+            this.Hello_textBox1.Name = "Hello_textBox1";
+            this.Hello_textBox1.Size = new System.Drawing.Size(598, 52);
+            this.Hello_textBox1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Algerian", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Location = new System.Drawing.Point(424, 429);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dateTimePicker1.Size = new System.Drawing.Size(462, 29);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dateTimePicker2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(715, 428);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(124, 30);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "";
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 7F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(-6, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "My First Windows Form App";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 323);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FloralWhite;
+            this.label2.Font = new System.Drawing.Font("Pristina", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(99, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(756, 61);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "made by: VIKTOR ZAFIROVSKI G1 - \"S.E.D.C\"";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label2.UseCompatibleTextRendering = true;
+            this.label2.UseMnemonic = false;
+            this.label2.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 457);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Hello_textBox1);
+            this.Controls.Add(this.ClickMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClickMe;
+        private System.Windows.Forms.TextBox Hello_textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ First_Win/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMe_Click(object sender, EventArgs e)
+        {
+            Hello_textBox1.Text = "Hello finaly... GOCE. Here is VIKTOR!!!";
+            Hello_textBox1.BackColor = Color.FromArgb(82, 3, 160);
+        }
+    }
+}
